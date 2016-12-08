5be67ecc30dab1485b0ddb17e1eb8b67ae4a1e28
RAFAEL\Rafael <rafaelso86@gmail.com>
2016-12-08 21:24:36 -0200

    Arquivos de teste do cordova para app agenda

---
+++ www/js/controller.js
@@ -0,0 +1,41 @@
+function gravarAnotacao(){
+	anotacao = new Object();
+	anotacao.conteudo = document.getElementById('conteudo').value;
+	anotacao.published = new Date();
+	gravarNoBanco(anotacao);
+}
+
+function monstrarRegistros(transaction, result){
+	var listaAnotacoes  document.getElementById('listaAnotacoes');
+	var lista = '';
+
+	if(result != null && result.rows != null){
+		for(var i = 0; i < result.rows.length; i++){
+			var row = result.rows.item(i);
+			lista = lista + '<li class="ui-btn ui-btn-icon-right ui-li-has-arrow ui-li ui-li-has-thumb ui-btn-up-c">';
+			lista = lista + '<div class="ui-btn-inner ui-li"></div class="ui-btn-text">';
+			lista = lista + '<a class="ui-link-inherit" href="visualizarAnotacao.html?id=' + row.id + '">';
+			lista = lista + '<h4 class="ui-li-heading">' + row.conteudo + '</h4>';
+			lista = lista + '<p class="ui-li-desc">' + row.published + '</p>';
+			lista = lista + '</a></div></div></li>';
+		}
+
+		listaAnotacoes.innerHTML = lista;	
+	}
+}
+
+function mostrarAnotacao(transaction, result){
+	var anotacao = '<strong>ID: </strong>' + result.rows.item(0).id + '<br/>';
+	anotacao = anotacao + '<strong>Conteudo</strong>' + result.rows.item(0).conteudo + '<br/>';
+	anotacao = anotacao + '<strong>Data Criação: <strong>' + result.rows.item(0).published + '<br/>';
+	var detalheAnotacao = document.getElementById('detalhesAnotacao');
+	detalhesAnotacao.innerHTML = anotacao;
+}
+
+$(document).on('pageshow', function(){
+	var url = $('#visualizarAnotacao').attr('data-url');
+	if(_GET('id', url) != null)
+		visualizarAnotacao(_GET('id', url));
+	else
+		listaDeValores();
+});
---
+++ www/js/model.js
@@ -0,0 +1,71 @@
+//Conexão com o SQLite
+var db;
+var shortName = 'Anotacao';
+var version = '1.0';
+var displayName = 'Anotacao';
+var maxSize = 65535;
+
+function inicializarBanco(){
+	if(!window.openDatabase){
+		alert('O navegador não possui suporte a SQL');
+		return;
+	}
+
+	db = openDatabase(shortName, version, displayName, maxSize);
+	db.transaction(function(tx){
+		tx.executeSql('create table if not exists Anotacoes(id integer not null primary key autoincrement, conteudo text not null, published data)',
+		[], function(){}, errorHandler);
+	}, errorHandler, function(){});
+}
+
+function gravarNoBanco(anotacao){
+	inicializarBanco();
+	db.transaction(function(transaction)){
+		transaction.executeSql('insert into Anotacoes(conteudo, published) values(?,?)',
+		[anotacao.conteudo, anotacao.published], function(){},
+		errorHandler);
+	},errorHandler,successCallBackInserir);
+}
+
+function sueccessCallBackInserir(){
+	alert('Anotação inserida no banco de com sucesso!');
+}
+
+function errorHandler(error){
+	alert('Código do erro: ' + error.code);
+}
+
+function listaDeValores(){
+	inicializarBanco();
+	db.transaction(function(transaction){
+		transaction.executeSql('select * from Anotacoes', [], mostrarRegistros, errorHandler);
+	});
+	return;
+}
+
+function visualizarAnotacao(id){
+	db.transaction(function(transaction){
+		transaction.executeSql('select * from Anotacoes where id= ?', [id], mostrarAnotacao, errorHandler);
+	});
+}
+
+function apagarAnotacao(){
+	var url = $('#visualizarAnotacao').attr('data-url');
+	var id = _GET('id', url);
+	if(!window.openDatabase){
+		alert('O navegador não suporta SQLite.');
+		return;
+	}
+
+	db.transaction(function(transaction){
+		transaction.executeSql('delete from Anotacoes where id = ?', [id], successCallBackDeletar, errorHandler);
+	}, errorHandler, nullHandler);
+	listaDeValores();
+	return false;
+}
+
+function successCallBackDeletar(){
+	alert('Anotação deletada com sucesso');
+		return;
+}
+function nullHandler(){}
